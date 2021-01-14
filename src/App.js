e8cf70fae6f65d8a6a3f1985a2a43021a4965d27
Tram Bui <pbui17@cmc.edu>
2021-01-14 13:18:26 -0600

    create dropdown of boards

--- src/App.js
+++ src/App.js
@@ -8,11 +8,43 @@ import axios from 'axios';
 
 const App = () => {
 
+  const [boardList, setBoardList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    axios.get('https://inspiration-board.herokuapp.com/boards')
+      .then((res) => {
+        // res.data = array of objects
+        const apiBoards = []
+        
+        res.data.map(singleBoard => {
+          apiBoards.push(singleBoard.board)
+        })
+       
+        setBoardList(apiBoards)
+      })
+      .catch((err) => {
+        setErrorMessage(err.message);
+      });
+  }, []);
+
+  const boardNames = [];
+  boardList.map(board => 
+    boardNames.push(<option value={board.name}> {board.name} </option>)
+  )
+
+
   return (
     <section>
       <header className="header">
-        <h1 className="header__h1"><span className="header__text">{emoji.getUnicode("heart_eyes")} Inspiration Board {emoji.getUnicode("heart_eyes")}</span></h1>
+        <h1 className="header__h1"><span className="header__text"> Inspiration Board</span></h1>
       </header>
+      <form>
+        <label>Boards:</label>
+        <select>
+          {boardNames}
+        </select>
+      </form>
       <Board
         url="https://inspiration-board.herokuapp.com/boards/"
         boardName={`tram-bui`}
